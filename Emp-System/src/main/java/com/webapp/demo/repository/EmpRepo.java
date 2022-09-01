fff2e10d5f1705d58a2f3f08207c4c0a7d7971e0
Mohamed Faraaz <76440943+faraaz1604@users.noreply.github.com>
2022-09-01 11:29:44 +0530

    Add files via upload

---
+++ Emp-System/src/main/java/com/webapp/demo/repository/EmpRepo.java
@@ -0,0 +1,11 @@
+package com.webapp.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.webapp.demo.entity.Employee;
+
+@Repository
+public interface EmpRepo extends JpaRepository<Employee, Integer>{
+
+}
